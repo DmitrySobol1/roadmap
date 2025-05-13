519a1a415b5d9d7c0a2492213154d85caf4a4108
DmitrySobol1 <saibleonetwo@gmail.com>
2025-05-13 03:27:36 +0300

    12-05_7

--- src/pages/Onboarding/Onboarding.tsx
+++ src/pages/Onboarding/Onboarding.tsx
@@ -1,24 +1,80 @@
-import { Section, List } from '@telegram-apps/telegram-ui';
-import type { FC } from 'react';
+import { Section, List, Steps, Cell, Button } from '@telegram-apps/telegram-ui';
 
+import type { FC } from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { Page } from '@/components/Page.tsx';
 
+import onboardingImg1 from '../../img/onb1.jpg';
+import onboardingImg2 from '../../img/onb2.jpg';
+import onboardingImg3 from '../../img/onb3.jpg';
 
+import styles from './Onboarding.module.css';
 
 export const Onboarding: FC = () => {
+  const [step, setStep] = useState(1);
+  const navigate = useNavigate();
 
+  function mainBtnListener() {
+    setStep(step + 1);
+    if (step == 3) {
+      navigate('/roadmap');
+    }
+  }
 
   return (
-    <Page back={false}>
+    <Page back={true}>
       <List>
-        
-        <Section
-          header="Тут будет онбординг"
-        //   footer="These pages help developer to learn more about current launch information"
-        >
-          
-          
+        <Section header="Что есть в приложении">
+          <Steps count={3} progress={step} />
+          {step == 1 && (
+            <Cell multiline>
+              <div className={styles.divImg}>
+                <img src={onboardingImg1} className={styles.onboardingImg} />
+              </div>
+              <p>
+                На вкладке «Roadmap» вы найдете подробную информацию для каждого
+                уровня разработчика
+              </p>
+            </Cell>
+          )}
+          {step == 2 && (
+            <Cell multiline>
+              <div className={styles.divImg}>
+                <img src={onboardingImg2} className={styles.onboardingImg2} />
+              </div>
+              <p>
+                Провалившись в конкретный уровень вы найдете инструкции, как
+                улучшить свои текущие навыки
+              </p>
+            </Cell>
+          )}
+          {step == 3 && (
+            <Cell multiline>
+              <div className={styles.divImg}>
+                <img src={onboardingImg3} className={styles.onboardingImg} />
+              </div>
+              <p>
+                На текущий момент полностью готов раздел «Roadmap разработчика».
+                «Архив материалов» и «Шаблоны проектов» станут доступны в
+                ближайшее время
+              </p>
+            </Cell>
+          )}
+
+          <div className={styles.btnDiv}>
+            <div className={styles.nextBtn}>
+              <Button
+                mode="filled"
+                size="m"
+                stretched
+                onClick={mainBtnListener}
+              >
+                Далее
+              </Button>
+            </div>
+          </div>
         </Section>
       </List>
     </Page>
--- src/pages/Roadmap/Roadmap.tsx
+++ src/pages/Roadmap/Roadmap.tsx
@@ -2,67 +2,80 @@ import type { FC } from 'react';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-
-import { Section, List, Cell, Divider, Image,Tabbar } from '@telegram-apps/telegram-ui';
+import {
+  Section,
+  List,
+  Cell,
+  Divider,
+  Image,
+  Tabbar,
+} from '@telegram-apps/telegram-ui';
 import { Link } from '@/components/Link/Link.tsx';
 import { Page } from '@/components/Page.tsx';
 
+
+
 import { Icon16Chevron } from '@telegram-apps/telegram-ui/dist/icons/16/chevron';
-import { Icon28Devices  } from '@telegram-apps/telegram-ui/dist/icons/28/devices'
+import { Icon28Devices } from '@telegram-apps/telegram-ui/dist/icons/28/devices';
 // import { Icon28Stats} from '@telegram-apps/telegram-ui/dist/icons/28/stats'
-import { Icon28Archive } from '@telegram-apps/telegram-ui/dist/icons/28/archive'
-import { Icon28Heart } from '@telegram-apps/telegram-ui/dist/icons/28/heart'
+import { Icon28Archive } from '@telegram-apps/telegram-ui/dist/icons/28/archive';
+import { Icon28Heart } from '@telegram-apps/telegram-ui/dist/icons/28/heart';
 
-
-import traineepic from '../../img/trainee.png'
-import junpic from '../../img/jun.png'
-import middlepic from '../../img/mid.png'
-import senpic from '../../img/sen.png'
+import traineepic from '../../img/trainee.png';
+import junpic from '../../img/jun.png';
+import middlepic from '../../img/mid.png';
+import senpic from '../../img/sen.png';
 
 // import styles from './Roadmap.module.css';
 
 export const Roadmap: FC = () => {
-
-const tabs = [
+  const tabs = [
     {
-      id:1,
-      text:'Roadmap',
-      Icon: Icon28Heart
+      id: 1,
+      text: 'Roadmap',
+      Icon: Icon28Heart,
     },
     {
-      id:2,
-      text:'Материалы',
-      Icon: Icon28Devices
+      id: 2,
+      text: 'Материалы',
+      Icon: Icon28Devices,
     },
     {
-      id:3,
-      text:'Шаблоны',
-      Icon: Icon28Archive
-    }
-
-  ]
+      id: 3,
+      text: 'Шаблоны',
+      Icon: Icon28Archive,
+    },
+  ];
 
   const [currentTab, setCurrentTab] = useState(tabs[0].id);
   const navigate = useNavigate();
 
-  function changePage(id:number){
+  function changePage(id: number) {
     setCurrentTab(id);
-    if (id === 1){
-    navigate('/roadmap');
-    } else if (id === 2){
+    if (id === 1) {
+      navigate('/roadmap');
+    } else if (id === 2) {
       navigate('/materials');
-    } else if (id === 3){
-      navigate('/templates');
-    } 
+    } else if (id === 3) {
+      navigate('/onboarding');
+    }
   }
+
+ 
 
   return (
     <Page back={false}>
       <List>
-        <Section header="Roadmap разработчика чат-ботов">
+        <Section
+          header="Roadmap разработчика чат-ботов"
+          footer="нажмите на любой уровень, чтобы посмотреть рекомендации для повышения навыков"
+        >
+          <Divider />
+          <Divider />
+          <Divider />
           <Link to="/trainee">
             <Cell
-              before={<Image size={48} src={traineepic}/>}
+              before={<Image size={48} src={traineepic} />}
               after={<Icon16Chevron />}
               subtitle="только начал изучение"
             >
@@ -71,12 +84,13 @@ const tabs = [
           </Link>
 
           <Divider />
+          <Divider />
+          <Divider />
 
-         
           <Link to="/junior">
             <Cell
               // before={<Icon28Stats />}
-              before={<Image size={48} src={junpic}/>}
+              before={<Image size={48} src={junpic} />}
               after={<Icon16Chevron />}
               subtitle="знает стандартные функции"
             >
@@ -84,49 +98,48 @@ const tabs = [
             </Cell>
           </Link>
 
-          
-        
+          <Divider />
+          <Divider />
+          <Divider />
+
           <Link to="/middle">
             <Cell
-              before={<Image size={48} src={middlepic}/>}
+              before={<Image size={48} src={middlepic} />}
               after={<Icon16Chevron />}
               subtitle="делает любые интеграции"
             >
               Средний (middle)
             </Cell>
           </Link>
-        
+
+          <Divider />
+          <Divider />
+          <Divider />
+
           <Link to="/senior">
             <Cell
-              before={<Image size={48} src={senpic}/>}
+              before={<Image size={48} src={senpic} />}
               after={<Icon16Chevron />}
               subtitle="разрабатывает mini app"
             >
               Профи (senior)
             </Cell>
           </Link>
-        
-        
         </Section>
 
-
-      <Tabbar>
-        {tabs.map(({
-        id,
-        text,
-        Icon
-      }) => <Tabbar.Item key={id} text={text} selected={id === currentTab} onClick={()=>changePage(id)}>
-            <Icon/>
-          </Tabbar.Item>)}
-      </Tabbar>
-
-
+        <Tabbar>
+          {tabs.map(({ id, text, Icon }) => (
+            <Tabbar.Item
+              key={id}
+              text={text}
+              selected={id === currentTab}
+              onClick={() => changePage(id)}
+            >
+              <Icon />
+            </Tabbar.Item>
+          ))}
+        </Tabbar>
       </List>
-
-
-
-
-
     </Page>
   );
 };
